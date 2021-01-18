42243b56332a31fcd624a513fb1aaa5d2ae4516e
scottjordan0001 <scottjordan0001@gmail.com>
2021-01-18 16:07:49 +0530

    modify index file

--- index.php
+++ index.php
@@ -10,6 +10,6 @@
 	<p>People are at the center of everything we do. With empathy for the learner as our launching pad, we support faculty to bring their expertise together with what inspires, excites and engages their learners. This partnership is key to creating authentic experiences that advance learner success.</p>
 	
 	<button type="button" class="btn btn-large btn-block btn-primary">button</button>
-	
+	dsadsad
 </body>
 </html>
