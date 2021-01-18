6486892c5f82e87d67c7dd967e5834f134c6cfba
scottjordan0001 <scottjordan0001@gmail.com>
2021-01-18 15:12:00 +0530

    add file

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Learning Experience Design</title>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
+<body>
+	<h1>Project Git</h1>
+	<p>People are at the center of everything we do. With empathy for the learner as our launching pad, we support faculty to bring their expertise together with what inspires, excites and engages their learners. This partnership is key to creating authentic experiences that advance learner success.</p>
+	<p>People are at the center of everything we do. With empathy for the learner as our launching pad, we support faculty to bring their expertise together with what inspires, excites and engages their learners. This partnership is key to creating authentic experiences that advance learner success.</p>
+</body>
+</html>
